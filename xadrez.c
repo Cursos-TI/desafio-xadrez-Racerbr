9f1d66cddae27ade76a9e9b4b0c1dc0e40ab0b69
Racerbr <thiagovitoria120@gmail.com>
2025-04-16 15:10:32 -0300

    Update xadrez.c
    Nível Mestre (usando funções recursivas)

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- 
+
 int main() {
     
     int i = 0;
@@ -13,29 +13,55 @@ int main() {
     scanf("%d", &escolha);
 
     switch (escolha){
-    // Após o usuário escolher a primeira opção, a torre se movimenta 5 casas para a direita
+    
+        // Após o usuário escolher a primeira opção, a torre se movimenta 5 casas para a direita
+    
     case 1:
-        for (i; i < 5; i++){
-            printf("Torre: Direita\n");
+    
+        void moverTorre(int casas){
+            if (casas > 0) {
+                printf("Direita!\n");
+                moverTorre(casas - 1); 
+            }
         }
+   
+        moverTorre(5);
+        return 0;
+    
     break;
-    // Após o usuário escolher a segunda opção, o bispo se movimenta cinco casas na diagonal para cima e à direita
+    
+     // Após o usuário escolher a segunda opção, o bispo se movimenta cinco casas na diagonal para cima e à direita
+    
     case 2:
-        do{ 
-            printf("Bispo: Cima, Direita!\n");
-            i++;
-        } while (i < 5);
-    break;
-    // Após o usuário escolher a terceira opção, a rainha se movimenta oito casas para a esquerda
-    case 3:
-        while (i < 8){
-            printf("Rainha: Esquerda!\n");
-            i++;
+        
+        void moverBispo(int casas){
+             if (casas > 0){
+                printf("Bispo: Cima, Direita!\n");
+                moverBispo(casas - 1);
+             }
         }
+
+        moverBispo(5);
+        return 0;
+    
     break;
+    
+        // Após o usuário escolher a terceira opção, a rainha se movimenta oito casas para a esquerda
+    case 3:
+        void moverRainha(int casas){
+            if (casas > 0){
+                printf("Rainha: Esquerda!\n");
+                moverRainha(casas - 1);
+            }
+        }
+
+        moverRainha(8);
+        return 0;
+    
+    break;
+    
     case 4:
         
-        int movimentoCompleto = 1;
         int escolha;
 
         printf("### Você escolheu o cavalo, agora, para qual direção você quer movimentá-lo? ###\n");
@@ -46,47 +72,56 @@ int main() {
         scanf("%d", &escolha);
 
         switch (escolha){
+            
             case 1:
-        
-            while (movimentoCompleto--){
-                for (int i = 0; i < 2; i++){
+
+            for (int segundoMovimento = 0; segundoMovimento < 1; segundoMovimento++){
+                for (int primeiroMovimento = 0; primeiroMovimento <= 1; primeiroMovimento++){
                     printf("Vertical!\n");
                 }
                 printf("Direita!\n");
-         }
-        
+            }
             break;
+            
             case 2:
         
-                while (movimentoCompleto--){
-                    for (int i = 0; i < 2; i++){
-                        printf("Vertical!\n");
-            }
+            for (int segundoMovimento = 0; segundoMovimento < 1; segundoMovimento++){
+                for (int primeiroMovimento = 0; primeiroMovimento <= 1; primeiroMovimento++){
+                    printf("Vertical!\n");
+                }
                 printf("Esquerda!\n");
-         }
+             }
             break;
+            
             case 3:
         
-                while (movimentoCompleto--){
-                    for (int i = 0; i < 2; i++){
-                        printf("Horizontal!\n");
-            }
+            for (int segundoMovimento = 0; segundoMovimento < 1; segundoMovimento++){
+                for (int primeiroMovimento = 0; primeiroMovimento <= 1; primeiroMovimento++){
+                    printf("Horizontal!\n");
+                }
                 printf("Direita!\n");
-         }
+             }
+            
             break;
+            
             case 4:
         
-                while (movimentoCompleto--){
-                    for (int i = 0; i < 2; i++){
-                        printf("Horizontal!\n");
-            }
+            for (int segundoMovimento = 0; segundoMovimento < 1; segundoMovimento++){
+                for (int primeiroMovimento = 0; primeiroMovimento <= 1; primeiroMovimento++){
+                    printf("Horizontal!\n");
+                }
                 printf("Esquerda!\n");
-         }
+             }
+            
             break;
+            
             default:
-            printf("Opção inválida. Tente novamente\n");
+                
+                printf("Opção inválida. Tente novamente\n");
+            
             break;
         }
+    
     break;
 
     
